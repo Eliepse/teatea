812b3f5019dbdf5981f7c4a59155c157b054fdd0
Élie Meignan <contact@eliepse.fr>
2025-05-29 16:47:36 +0200

    Display paths parents

--- app/components/search/tea-search/resultItem.tsx
+++ app/components/search/tea-search/resultItem.tsx
@@ -3,10 +3,10 @@ import Leaf from "~/components/icons/leaf";
 
 export function ResultItem(props: { tea: Tea; onClick?: (tea: Tea) => void }) {
 	const hasName = !!props.tea.name;
+	const types = props.tea.parentTypes;
+	const origins = props.tea.origin ? [...props.tea.parentOrigins, props.tea.origin].slice(0, 2) : [];
 
-	// const typePath = props.tea.type.slice(0, hasName ? 0 : -1);
-
-	function onClick(e: MouseEvent) {
+	function handleClick(e: MouseEvent) {
 		e.stopPropagation();
 
 		if (props.onClick) {
@@ -15,21 +15,25 @@ export function ResultItem(props: { tea: Tea; onClick?: (tea: Tea) => void }) {
 	}
 
 	return (
-		<div className="flex px-4 py-2 border-t border-base-300 items-center">
+		<div className="flex px-4 py-2 border-t border-base-300 items-center" onClick={handleClick}>
 			<div className="flex-1">
-				{/*{0 !== typePath.length && <div>{typePath.map((t) => t.name).join(" · ")}</div>}*/}
-				{/*<div>{props.tea.name ?? props.tea.type.slice(-1)[0]?.name}</div>*/}
-				{hasName && <div className="text-xs text-base-content/50 mb-1">{props.tea.type.name}</div>}
+				{0 !== props.tea.parentTypes.length && (
+					<div className="text-xs text-base-content/50 mb-1">
+						{props.tea.parentTypes.map((t) => t.name).join(" · ")}
+					</div>
+				)}
 				<div className="font-semibold">{props.tea.name ?? props.tea.type.name}</div>
 			</div>
 			<div className="text-right text-xs">
-				{/*{!!props.tea.origin?.length && <div>{props.tea.origin.map((t) => t.name).join(" · ")}</div>}*/}
-				{!!props.tea.origin && <div className="mb-1">{props.tea.origin.name}</div>}
-				{!!props.tea.cultivar && (
-					<div className="text-base-content/50">
-						{props.tea.cultivar.name} <Leaf className="size-3 text-green-400 inline" />
-					</div>
-				)}
+				{0 !== origins.length && <div className="mb-1">{origins.map((t) => t.name).join(" · ")}</div>}
+				<div className="text-base-content/50">
+					&nbsp;
+					{!!props.tea.cultivar && (
+						<>
+							{props.tea.cultivar.name} <Leaf className="size-3 text-green-400 inline" />
+						</>
+					)}
+				</div>
 			</div>
 		</div>
 	);
