03a9acb7361329b97e7de990c1b973592256bc07
Élie Meignan <contact@eliepse.fr>
2025-07-04 19:32:06 +0200

    feat: add helper for buttons actions

--- pwa/app/utils/function.ts
+++ pwa/app/utils/function.ts
@@ -1,7 +1,17 @@
+import type { SyntheticEvent } from "react";
+
 export function warnNotImplemented(): void {
 	console.warn("Not implemented!");
 }
 
 export function throwNotImplemented(): never {
 	throw new Error("Not implemented");
+}
+
+export function handleUIEvent(callback: (e: SyntheticEvent) => void): (e: SyntheticEvent) => void {
+	return (e) => {
+		e.stopPropagation();
+		e.preventDefault();
+		return callback(e);
+	};
 }
