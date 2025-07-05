f61a851390cdfbea6161f2f577580d611cec1ef8
Élie Meignan <contact@eliepse.fr>
2025-07-05 15:37:39 +0200

    feat: support having minimum request delay for debug

--- pwa/app/utils/api.ts
+++ pwa/app/utils/api.ts
@@ -1,12 +1,14 @@
 import { TokenUtils } from "~/auth/hooks/useToken";
+import { wait } from "~/utils/time";
 
 type FetchApiConfig = Omit<RequestInit, "body"> & {
 	payload?: string | number | object | null;
 };
 
 export async function fetchApi(url: string, config?: FetchApiConfig): Promise<Response> {
+	const startedAt = Date.now();
 	const token = TokenUtils.get();
-	const fetchConfigs: RequestInit = {  ...config };
+	const fetchConfigs: RequestInit = { ...config };
 
 	fetchConfigs.headers = new Headers({
 		"Content-Type": "application/ld+json",
@@ -22,10 +24,30 @@ export async function fetchApi(url: string, config?: FetchApiConfig): Promise<Re
 		fetchConfigs.body = "string" === typeof config.payload ? config.payload : JSON.stringify(config.payload);
 	}
 
-	const response = await fetch(`${import.meta.env.PUBLIC_API_URL}${url}`, fetchConfigs);
+	let response;
+
+	try {
+		response = await fetch(`${import.meta.env.PUBLIC_API_URL}${url}`, fetchConfigs);
+	} catch (e) {
+		console.warn(`Request to ${url} failed`);
+		console.warn(e);
+		throw e;
+	}
 
 	if (401 === response.status) {
 		TokenUtils.clear();
+	}
+
+	const duration = Date.now() - startedAt;
+
+	// Debug: Add a fake delay to make sure requests duration
+	// is greater or equal to PUBLIC_API_FAKE_DELAY env
+	if (true === import.meta.env.DEV && !!import.meta.env.PUBLIC_API_FAKE_DELAY) {
+		const fakeDelayMs = parseInt(import.meta.env.PUBLIC_API_FAKE_DELAY);
+
+		if (fakeDelayMs > duration) {
+			await wait(fakeDelayMs - duration);
+		}
 	}
 
 	return response;
