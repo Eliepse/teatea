815940c3edc4790ff7a6da0fb0d0af773445df1a
Élie Meignan <contact@eliepse.fr>
2025-07-14 22:57:41 +0200

    feat: remove non supported features from dashboard

--- pwa/app/routes/dashboard.tsx
+++ pwa/app/routes/dashboard.tsx
@@ -29,16 +29,6 @@ export default function Dashboard() {
 				<button className="btn btn-block mt-6">Add a tea type</button>
 			</Link>
 
-			<Link to="/me/teas">
-				<button className="btn btn-block mt-6">My collection</button>
-			</Link>
-
-			<button className="btn btn-block mt-6" onClick={() => setBrewForm(true)}>
-				Brew
-			</button>
-
-			<BrewMultiStepForm open={brewForm} onClose={() => setBrewForm(false)} />
-
 			<button className="btn btn-block mt-6" onClick={() => setAddTeaForm(true)}>
 				Add tea
 			</button>
