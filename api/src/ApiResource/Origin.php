2740299c572001ffd8accc7969a0d1fbc4aa2ecf
Élie Meignan <contact@eliepse.fr>
2025-07-18 16:46:35 +0200

    feat: remove filter as subresource

--- api/src/ApiResource/Origin.php
+++ api/src/ApiResource/Origin.php
@@ -5,16 +5,9 @@ namespace App\ApiResource;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
-use ApiPlatform\Metadata\Link;
 use App\Doctrine\DBAL\Types\ValueObject\LTreePath;
 use App\State\OriginProvider;
-use App\State\TeaTypeProvider;
 
-#[GetCollection(
-	uriTemplate: "/origins/{originId}/tea_types",
-	uriVariables: ['originId' => new Link(fromClass: Origin::class)],
-	provider: TeaTypeProvider::class),
-]
 #[Get(provider: OriginProvider::class)]
 #[GetCollection(provider: OriginProvider::class)]
 class Origin
