796a0620b41b4e4c637ef650ec65ca8280d55afd
Élie Meignan <contact@eliepse.fr>
2025-08-01 18:23:30 +0200

    feat: add loader to layout

--- pwa/app/layouts/AuthLayout.tsx
+++ pwa/app/layouts/AuthLayout.tsx
@@ -2,13 +2,29 @@ import type { PropsWithChildren, ReactNode } from "react";
 import clsx from "clsx";
 import styles from "./AuthLayout.module.css";
 import { CalendarDaysIcon, HomeIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router";
+import { Link, useNavigation } from "react-router";
+import Leaf from "~/components/icons/leaf";
 
 export function AuthLayout(props: PropsWithChildren<{ className?: string; activeKey?: string }>) {
+	const navigation = useNavigation();
+	const isNavigating = Boolean(navigation.location);
+
 	return (
 		<div className={styles.root}>
-			<div className={clsx(styles.body, props.className)}>{props.children}</div>
-			<nav className="border-t border-gray-100">
+			{isNavigating && (
+				<div className="flex items-center justify-center">
+					<div className="flex justify-center text-gray-500">
+						<span className="inline-block animate-pulse">
+							<Leaf className="size-6 rotate-90" />
+						</span>
+						<span className="ml-2">Loading...</span>
+					</div>
+				</div>
+			)}
+
+			{false === isNavigating && <div className={clsx(styles.body, props.className)}>{props.children}</div>}
+
+			<nav className="border-t border-gray-200">
 				<ul className="w-full h-full flex px-4">
 					<li className="flex-1">
 						<Link to="/welcome">
