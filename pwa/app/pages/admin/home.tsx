5bf19a4203d5ba6664dd34a906dcdc192347ef31
Élie Meignan <contact@eliepse.fr>
2025-08-22 16:45:39 +0200

    feat: add back button to regular dashboard

--- pwa/app/pages/admin/home.tsx
+++ pwa/app/pages/admin/home.tsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { PageLayout } from "~/components/shared/paged/PageLayout";
 
 export default function AdminHomePage() {
+	const navigate = useNavigate();
+
 	return (
-		<PageLayout title="Admin">
+		<PageLayout title="Admin" onBack={() => navigate("/welcome")}>
 			<Link to="/admin/members" className="btn btn-block">
 				Members
 			</Link>
