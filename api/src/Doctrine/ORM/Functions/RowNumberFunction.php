3be346b17be197d8b53721090cd7c17de4b617dc
Élie Meignan <contact@eliepse.fr>
2025-08-24 22:09:28 +0200

    feat: support ROW_NUMBER postgres window function

---
+++ api/src/Doctrine/ORM/Functions/RowNumberFunction.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Doctrine\ORM\Functions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\AST\Node;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
+
+/**
+ * Add support for Postgres ROW_NUMBER window function
+ * @see https://www.postgresql.org/docs/17/functions-window.html
+ */
+class RowNumberFunction extends FunctionNode
+{
+	protected Node $windowDefinition;
+	protected Node $second;
+
+	public function getSql(SqlWalker $sqlWalker): string
+	{
+//		dd($this->windowDefinition, $this->windowDefinition->dispatch($sqlWalker));
+		return sprintf(
+			"ROW_NUMBER() OVER (%s)",
+			$this->windowDefinition->dispatch($sqlWalker),
+		);
+	}
+
+	public function parse(Parser $parser): void
+	{
+		$parser->match(TokenType::T_IDENTIFIER);
+		$parser->match(TokenType::T_OPEN_PARENTHESIS);
+
+		$this->windowDefinition = $parser->OrderByClause();
+
+		$parser->match(TokenType::T_CLOSE_PARENTHESIS);
+	}
+}
