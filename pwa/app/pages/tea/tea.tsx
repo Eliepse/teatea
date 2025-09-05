7067f9290dd8bff49fc3aa072ec9328b16c262f5
Élie Meignan <contact@eliepse.fr>
2025-09-05 15:11:56 +0200

    style: add more colors

--- pwa/app/pages/tea/tea.tsx
+++ pwa/app/pages/tea/tea.tsx
@@ -11,12 +11,22 @@ import { formatDistanceToNow } from "date-fns";
 import Leaf from "~/components/icons/leaf";
 import WaterDrop from "~/components/icons/WaterDrop";
 import { limit } from "~/utils/text";
+import clsx from "clsx";
 
 export async function clientLoader(args: Route.ClientLoaderArgs) {
 	const tea = await (await getApi<Tea>(`/teas/${args.params.id}`)).json();
 	const stats = await (await getApi<TeaStats>(`/teas/${args.params.id}/stats`)).json();
 	return { tea, stats };
 }
+
+const TEA_FAMILY_BADGE_CLS = {
+	yellow: "badge-warning",
+	white: "badge-neutral badge-outline bg-white",
+	green: "badge-success",
+	wulong: "badge-info",
+	black: "badge-error text-white",
+	fermented: "badge-neutral",
+} as const;
 
 export default function TeaPage(props: Route.ComponentProps) {
 	const { tea, stats } = props.loaderData;
@@ -36,43 +46,49 @@ export default function TeaPage(props: Route.ComponentProps) {
 
 	return (
 		<div>
-			<header className="p-4">
+			<header className="p-4 bg-base-200">
 				<button className="btn btn-ghost p-0 mb-4" onClick={handleUIEvent(() => navigate(-1))}>
 					<ArrowLeftIcon className="size-4 mr-1" /> Back
 				</button>
 
-				<div className="mb-2">
-					<div className="text-base-content/60 mb-1">
-						{!!tea.originPath && <FormatOriginPath originPath={tea.originPath} />}
-					</div>
-					<h1 className="text-3xl">{tea.type?.name ?? familyLabel}</h1>
-				</div>
+				<h1 className="text-3xl mb-2">{tea.type?.name ?? familyLabel}</h1>
 
-				<ul>
+				<ul className="flex gap-2">
 					{!!tea.type && (
 						<li>
-							<div className="badge badge-outline badge-accent">{tea.family} tea</div>
+							<div className={clsx("badge", TEA_FAMILY_BADGE_CLS[tea.family])}>{tea.family} tea</div>
+						</li>
+					)}
+
+					{!!tea.originPath && (
+						<li>
+							<div className="badge badge-soft badge-neutral">
+								<FormatOriginPath originPath={tea.originPath} />
+							</div>
 						</li>
 					)}
 				</ul>
 			</header>
+
 			<main>
-				<section className="px-4 mt-4">
-					<div className="stats shadow w-full">
-						<div className="stat">
-							<div className="stat-value">{stats.drinksCount}</div>
-							<div className="stat-desc">tea sessions</div>
-						</div>
-						<div className="stat">
-							<div className="stat-value">{stats.drinkersCount}</div>
-							<div className="stat-desc">people tried</div>
-						</div>
+				{0 !== stats.drinkersCount && (
+					<div className="px-4 mt-4">
+						<p>
+							This tea has been brewed{" "}
+							<strong>
+								{stats.drinksCount}&nbsp;{1 < stats.drinksCount ? "times" : "time"}
+							</strong>{" "}
+							times by a total of{" "}
+							<strong>
+								{stats.drinkersCount}&nbsp;{1 < stats.drinkersCount ? "members" : "member"}.
+							</strong>
+						</p>
 					</div>
-				</section>
+				)}
 
 				{0 !== (drinksQuery.data?.member?.length ?? 0) && (
 					<section className="px-4 mt-8">
-						<h2 className="text-lg mb-4">Other's drink</h2>
+						<h2 className="text-lg mb-4">How others brewed it?</h2>
 						<ul>
 							{drinksQuery.data?.member?.map((drink) => (
 								<li className="mb-2">
@@ -80,14 +96,14 @@ export default function TeaPage(props: Route.ComponentProps) {
 										<div className="flex text-xs gap-2 items-center">
 											{!!drink.teaQuantity && (
 												<div className="flex justify-between items-center rounded-md border leading-1 border-gray-400 p-1.5">
-													<Leaf className="size-3 text-base-content/40 mr-2" />
+													<Leaf className="size-3 text-green-300 mr-2" />
 													<span>{`${drink.teaQuantity} g`}</span>
 												</div>
 											)}
 
 											{!!drink.waterMl && (
 												<div className="flex justify-between items-center rounded-md border leading-1 border-gray-400 p-1.5">
-													<WaterDrop className="size-3 text-base-content/40 mr-2" />
+													<WaterDrop className="size-3 text-blue-300 mr-2" />
 													<span>{`${drink.waterMl} ml`}</span>
 												</div>
 											)}
