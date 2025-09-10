7cea7966312627f0f67f409c80f8c280bdf9f383
Élie Meignan <contact@eliepse.fr>
2025-09-10 22:47:32 +0200

    feat: support removing duplicates on pluck helper

--- api/src/Helper/Arr.php
+++ api/src/Helper/Arr.php
@@ -4,9 +4,9 @@ namespace App\Helper;
 
 final readonly class Arr
 {
-	public static function pluck(array $array, string|callable $key): array
+	public static function pluck(array $array, string|callable $key, bool $unique = false): array
 	{
 		$extractor = is_string($key) ? fn($i) => is_array($i) ? $i[$key] : $i->$key : $key;
-		return array_map($extractor, $array);
+		return $unique ? array_unique(array_map($extractor, $array)) : array_map($extractor, $array);
 	}
 }
