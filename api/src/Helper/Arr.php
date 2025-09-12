123135247980c8a37c65d8b58b61a33f4ab26104
Élie Meignan <contact@eliepse.fr>
2025-09-12 11:27:03 +0200

    feat: add keyBy array helper

--- api/src/Helper/Arr.php
+++ api/src/Helper/Arr.php
@@ -9,4 +9,20 @@ final readonly class Arr
 		$extractor = is_string($key) ? fn($i) => is_array($i) ? $i[$key] : $i->$key : $key;
 		return $unique ? array_unique(array_map($extractor, $array)) : array_map($extractor, $array);
 	}
+
+	/**
+	 * @template T
+	 * @param array<T> $array
+	 * @param string|(callable(T $item): mixed) $key
+	 *
+	 * @return array
+	 */
+	public static function keyBy(array $array, string|callable $key): array
+	{
+		$extractor = is_string($key) ? fn($item) => (is_object($item) ? $item->{$key} : $item[$key]) : $key;
+		return array_reduce($array, function ($map, $item) use ($extractor) {
+			$map[$extractor($item)] = $item;
+			return $map;
+		}, []);
+	}
 }
