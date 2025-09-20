d946988b5db3e426b4a9d5fb1e5b9a8abc6f5daa
Élie Meignan <contact@eliepse.fr>
2025-09-20 11:35:16 +0200

    feat: allow Iri on isAuthor hook

--- pwa/app/auth/components/voters/IfAuthor.tsx
+++ pwa/app/auth/components/voters/IfAuthor.tsx
@@ -1,16 +1,28 @@
 import type { PropsWithChildren } from "react";
 import { useUser } from "~/auth/hooks/useUser";
-import type { User } from "~t/types";
+import type { Iri, User } from "~t/types";
 
 type Author = Partial<Pick<User, "username" | "id" | "@id">>;
 
-export function IfAuthor(props: PropsWithChildren<{ author?: Author }>) {
-	return useIsAuthor(props.author);
+export function IfAuthor(props: PropsWithChildren<{ author: Author | undefined } | { iri: Iri | undefined }>) {
+	if ("author" in props) {
+		return useIsAuthor(props.author);
+	}
+
+	if ("iri" in props) {
+		return useIsAuthor(props.iri);
+	}
+
+	return null;
 }
 
-export function useIsAuthor(author?: Author) {
+export function useIsAuthor(author?: Author | Iri) {
 	const user = useUser();
 	const userIri = user.data ? `/members/${user.data.username}` : undefined;
+
+	if (typeof author === "string") {
+		return author === userIri;
+	}
 
 	if (undefined !== author?.["@id"] && author["@id"] === userIri) {
 		return true;
