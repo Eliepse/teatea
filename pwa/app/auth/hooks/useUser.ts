973614da59e94578b9d33b288402ef4df73ed0ba
Élie Meignan <contact@eliepse.fr>
2025-09-20 10:31:02 +0200

    feat: remove auto-redirect with user hook

--- pwa/app/auth/hooks/useUser.ts
+++ pwa/app/auth/hooks/useUser.ts
@@ -1,8 +1,6 @@
 import type { User } from "~t/types";
 import { fetchApi } from "~/utils/api";
 import { useQuery } from "@tanstack/react-query";
-import { UnauthenticatedError } from "~/auth/errors/UnauthenticatedError";
-import { useNavigate } from "react-router";
 
 async function fetchSelf(): Promise<User> {
 	return await (await fetchApi<User>("/members/me")).json();
@@ -11,18 +9,10 @@ async function fetchSelf(): Promise<User> {
 const QUERY_TTL = 15 * 60 * 1000;
 
 export function useUser() {
-	const navigate = useNavigate();
-	const query = useQuery({
+	return useQuery({
 		queryFn: fetchSelf,
 		queryKey: ["user:me"],
 		staleTime: QUERY_TTL,
 		gcTime: QUERY_TTL,
 	});
-
-	if (query.error instanceof UnauthenticatedError) {
-		navigate("/login");
-		return query;
-	}
-
-	return query;
 }
