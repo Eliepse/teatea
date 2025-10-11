036bc046d520259b66e820e3be93034b20885159
Élie Meignan <contact@eliepse.fr>
2025-10-11 12:31:12 +0200

    feat: add username to jwt payload

---
+++ api/src/Listener/CustomizeJWTokenListener.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Listener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+
+final class CustomizeJWTokenListener
+{
+	#[AsEventListener("lexik_jwt_authentication.on_jwt_created")]
+	public function onJWTCreated(JWTCreatedEvent $event): void
+	{
+		$user = $event->getUser();
+		assert($user instanceof User);
+
+		$event->setData(
+			array_replace(
+				$event->getData(),
+				["username" => $user->username],
+			),
+		);
+	}
+}
