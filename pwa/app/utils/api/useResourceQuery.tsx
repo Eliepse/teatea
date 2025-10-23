a60f8e77df48d2c9e424649c8be75297528b2501
Élie Meignan <contact@eliepse.fr>
2025-10-23 22:31:45 +0200

    fix: null value not supported

--- pwa/app/utils/api/useResourceQuery.tsx
+++ pwa/app/utils/api/useResourceQuery.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchApi } from "~/utils/api";
 import type { Iri, Resource } from "~t/types";
 
-export function useResourceQuery<T extends Resource>(iri: Iri | undefined) {
+export function useResourceQuery<T extends Resource>(iri: Iri | undefined | null) {
 	return useQuery({
 		queryFn: async (ctx) => {
 			if (!ctx.queryKey[0]) {
