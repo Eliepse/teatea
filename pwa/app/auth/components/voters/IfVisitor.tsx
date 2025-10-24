51a8112ea6ff0444de7b777719289750e547568f
Élie Meignan <contact@eliepse.fr>
2025-10-24 11:45:04 +0200

    feat: add visitor voter component

--- pwa/app/auth/components/voters/IfVisitor.tsx
+++ pwa/app/auth/components/voters/IfVisitor.tsx
@@ -1,7 +1,7 @@
 import type { PropsWithChildren } from "react";
 import { useToken } from "~/auth/hooks/useToken";
 
-export function IfAuthenticated(props: PropsWithChildren) {
+export function IfVisitor(props: PropsWithChildren) {
 	const [token] = useToken();
-	return null === token ? null : props.children;
+	return null === token ? props.children : null;
 }
