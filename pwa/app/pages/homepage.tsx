f1da888fd4b0eeef30fe03b276a336e25cab2b3f
Élie Meignan <contact@eliepse.fr>
2025-10-24 11:45:28 +0200

    style: create nicer temporary homepage

--- pwa/app/pages/homepage.tsx
+++ pwa/app/pages/homepage.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import { TokenUtils } from "~/auth/hooks/useToken";
+import { TokenUtils, useToken } from "~/auth/hooks/useToken";
 import type { Route } from "../../.react-router/types/app/pages/+types/homepage";
 
 export async function clientLoader() {
@@ -9,17 +9,21 @@ export async function clientLoader() {
 }
 
 export default function Homepage(props: Route.ComponentProps) {
+	const [token] = useToken();
+
 	return (
-		<div className="p-6">
-			{props.loaderData.isAuth ? (
-				<Link to="/welcome">
-					<button className="btn btn-block">Dashboard</button>
+		<div className="p-8 flex flex-col h-screen bg-[url(img/tea-tree-leaves-field.jpg)] bg-cover">
+			<div className="flex-1 flex flex-col justify-center text-center text-green-50">
+				<h1 className="text-7xl font-bold font-serif mb-6">teatea</h1>
+				<p className="text-lg max-w-xs mx-auto">Your personal tea journal</p>
+			</div>
+
+			<div className="mt-8">
+				<Link to={token ? "/welcome" : "/login"} className="btn btn-lg btn-block">
+					Open my tea journal
 				</Link>
-			) : (
-				<Link to="/login">
-					<button className="btn btn-block">Login</button>
-				</Link>
-			)}
+				<p className="mt-4 text-xs text-white text-center">This app is currently in development</p>
+			</div>
 		</div>
 	);
 }
