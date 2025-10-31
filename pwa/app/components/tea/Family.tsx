85730ede0684f08ed1fb22f4072b6766c511da70
Élie Meignan <contact@eliepse.fr>
2025-10-31 14:52:09 +0100

    feat: support inverted tea family icon

--- pwa/app/components/tea/Family.tsx
+++ pwa/app/components/tea/Family.tsx
@@ -11,11 +11,14 @@ const ICON_COLOR = {
 	fermented: "text-stone-500",
 } as const;
 
-export function Family(props: { family: TeaFamily; iconOnly?: boolean; className?: string }) {
+export function Family(props: { family: TeaFamily; iconLast?: boolean; iconOnly?: boolean; className?: string }) {
+	const leaf = <Leaf className={clsx("size-[.75em]", ICON_COLOR[props.family])} />;
+	const text = !props.iconOnly && <span className={props.iconLast ? "mr-1" : "ml-1"}>{props.family}</span>;
+
 	return (
 		<span className={clsx("inline-flex items-center", props.className)}>
-			<Leaf className={clsx("size-[.75em]", ICON_COLOR[props.family])} />
-			{!props.iconOnly && <span className="ml-1">{props.family}</span>}
+			{props.iconLast ? text : leaf}
+			{props.iconLast ? leaf : text}
 		</span>
 	);
 }
