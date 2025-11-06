6f78579cb58bd112c99f853ad0ca5223588d216a
Élie Meignan <contact@eliepse.fr>
2025-11-06 21:01:48 +0100

    style: refresh tea list page

--- pwa/app/pages/lists/teaList.tsx
+++ pwa/app/pages/lists/teaList.tsx
@@ -1,10 +1,11 @@
 import type { Route } from "../../../.react-router/types/app/pages/lists/+types/teaList";
 import { getApi } from "~/utils/api";
 import type { ApiCollection, MemberTea, TeaList } from "~t/types";
-import { PageLayout } from "~/components/shared/paged/PageLayout";
 import { Link, useNavigate } from "react-router";
 import { useQuery } from "@tanstack/react-query";
 import { TeaShortCard } from "~/components/tea/TeaShortCard";
+import { BackButton } from "~/components/shared/navigation/BackButton";
+import { TeaCard } from "~/components/tea/TeaCard";
 
 export async function clientLoader(args: Route.ClientLoaderArgs) {
 	const id = args.params.id;
@@ -18,30 +19,37 @@ export default function TeaListPage(props: Route.ComponentProps) {
 
 	const teaLinks = useQuery({
 		queryFn: async (ctx) => {
-			let id = ctx.queryKey[1];
+			const id = ctx.queryKey[1];
 			const response = await getApi<ApiCollection<MemberTea>>(`/lists/${id}/teas`);
 			return await response.json();
 		},
 		queryKey: ["tea-list", props.params.id],
 	});
 
-	return <PageLayout onBack={() => navigate(-1)} title={props.loaderData.name}>
-		<ul>
-			{teaLinks.data?.member?.map((teaLink) => (
-				<li key={teaLink.id} className="mb-2">
-					<Link to={`/tea/${teaLink.tea.id}`}>
-						<TeaShortCard
-							family={teaLink.tea.family}
-							type={teaLink.tea.type}
-							path={teaLink.tea.originPath}
-							cultivar={teaLink.tea.cultivar}
-							year={teaLink.tea.year}
-							roast={teaLink.tea.roast}
-							className="bg-slate-100"
-						/>
-					</Link>
-				</li>
-			))}
-		</ul>
-	</PageLayout>;
+	return (
+		<div className="p-4 pb-8 bg-green-50 min-h-dvh">
+			<header className="mb-8 pt-2 relative">
+				<BackButton className="mr-auto shadow-sm absolute top-0 left-0" />
+				<h1 className="text-3xl font-bold font-header text-center text-green-900">{props.loaderData.name}</h1>
+			</header>
+			<ul>
+				{teaLinks.data?.member?.map((teaLink) => (
+					<li key={teaLink.id} className="mb-2">
+						<Link to={`/tea/${teaLink.tea.id}`}>
+							<TeaCard
+								teaId={teaLink.tea.id}
+								family={teaLink.tea.family}
+								roast={teaLink.tea.roast}
+								type={teaLink.tea.type}
+								year={teaLink.tea.year}
+								cultivar={teaLink.tea.cultivar}
+								origin={teaLink.tea.originPath}
+								className="bg-white shadow-sm"
+							/>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
