323ec891939192308b4ab2ae7fd53f52f1c984a9
Élie Meignan <contact@eliepse.fr>
2025-11-06 18:34:57 +0100

    feat: redesign tasted tea page with more details

--- pwa/app/pages/tea/tea-list.tsx
+++ pwa/app/pages/tea/tea-list.tsx
@@ -1,13 +1,12 @@
 import { AuthLayout } from "~/layouts/AuthLayout";
-import { handleUIEvent } from "~/utils/function";
-import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { Link, useNavigate } from "react-router";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getApi } from "~/utils/api";
 import type { ApiPaginatedCollection } from "~t/types";
 import { denormalizeTea, type TeaRaw } from "~/utils/api/normalization/tea";
 import { useUser } from "~/auth/hooks/useUser";
-import { TeaShortCard } from "~/components/tea/TeaShortCard";
+import { BackButton } from "~/components/shared/navigation/BackButton";
+import { TeaCard } from "~/components/tea/TeaCard";
 
 export default function TeaListPage() {
 	const navigate = useNavigate();
@@ -36,17 +35,14 @@ export default function TeaListPage() {
 	const hasTeas = !!teasQuery.data && 0 !== teasQuery.data.pages.length;
 
 	return (
-		<AuthLayout className="pb-8">
-			<header className="p-4 mb-4">
-				<button className="btn btn-ghost p-0 mb-4" onClick={handleUIEvent(() => navigate(-1))}>
-					<ArrowLeftIcon className="size-4 mr-1" /> Back
-				</button>
-
-				<h1 className="text-xl">Teas you already tasted</h1>
+		<AuthLayout className="p-4 pb-8 bg-green-50">
+			<header className="mb-8 pt-2 relative">
+				<BackButton className="mr-auto shadow-sm absolute top-0 left-0" />
+				<h1 className="text-3xl font-bold font-header text-center">Tasted teas</h1>
 			</header>
 
 			{!teasQuery.isPending && !hasTeas && (
-				<p className="px-8 mt-8 text-center text-base-content/80">
+				<p className="px-4 mt-8 text-center text-base-content/80">
 					You haven't tasted any tea yet. Ready to{" "}
 					<Link className="link link-primary" to="/session/new">
 						record your first session?
@@ -55,7 +51,7 @@ export default function TeaListPage() {
 			)}
 
 			{teasQuery.isPending && (
-				<div className="px-4">
+				<div>
 					<div className="h-16 skeleton mb-2" />
 					<div className="h-16 skeleton mb-2" />
 					<div className="h-16 skeleton mb-2" />
@@ -66,20 +62,21 @@ export default function TeaListPage() {
 			)}
 
 			{hasTeas && (
-				<div className="px-4">
-					<ul className="mb-4">
+				<div>
+					<ul className="grid gap-4 mb-4">
 						{teasQuery.data?.pages.map((page) =>
 							page.member.map((tea) => (
 								<li key={tea.id} className="mb-2">
 									<Link to={`/tea/${tea.id}`}>
-										<TeaShortCard
+										<TeaCard
+											teaId={tea.id}
 											family={tea.family}
-											type={tea.type}
-											path={tea.originPath}
-											cultivar={tea.cultivar}
-											year={tea.year}
 											roast={tea.roast}
-											className="bg-slate-100"
+											type={tea.type}
+											year={tea.year}
+											cultivar={tea.cultivar}
+											origin={tea.originPath}
+											className="bg-white"
 										/>
 									</Link>
 								</li>
