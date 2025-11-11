73b83e66bde2be550462c450f137b1414f84a38f
Élie Meignan <contact@eliepse.fr>
2025-11-11 22:23:44 +0100

    feat: add easy modal menu component

---
+++ pwa/app/components/shared/navigation/MenuModal.tsx
@@ -0,0 +1,45 @@
+import { Children, type PropsWithChildren, type ReactNode } from "react";
+import { Modal } from "~/components/shared/modal/Modal";
+import clsx from "clsx";
+import { handleUIEvent } from "~/utils/function";
+
+const ItemCls = {
+	default: "text-green-950 active:bg-teal-200 focus:bg-teal-100",
+	danger: "text-red-600 active:bg-red-200 focus:bg-red-100",
+} as const;
+
+export function MenuModal(props: PropsWithChildren<{ open: boolean; onClose: () => void }>) {
+	return (
+		<Modal onClose={props.onClose} open={props.open} position="bottom" className="p-0 text-lg">
+			<ul aria-live="polite">
+				{Children.map(props.children, (child) => (
+					<li className="not-first:border-t border-teal-200">{child}</li>
+				))}
+			</ul>
+		</Modal>
+	);
+}
+
+export function MenuItem(props: {
+	danger?: boolean;
+	label: ReactNode;
+	onClick: () => void;
+	disabled?: boolean;
+	icon?: ReactNode;
+}) {
+	return (
+		<button
+			className={clsx(
+				"flex items-center w-full pl-6 pr-8 h-16",
+				"disabled:text-teal-500 disabled:bg-teal-50",
+				!props.disabled && ItemCls[props.danger ? "danger" : "default"],
+				// 0 === i && "pt-1 h-17",
+			)}
+			onClick={handleUIEvent(props.onClick)}
+			disabled={props.disabled}
+		>
+			{props.label}
+			{!!props.icon && <span className="ml-auto">{props.icon}</span>}
+		</button>
+	);
+}
