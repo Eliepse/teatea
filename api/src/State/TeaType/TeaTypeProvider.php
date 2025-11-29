77d03e46d87438858e2cd1bcd5e0a1c5ea122083
Élie Meignan <contact@eliepse.fr>
2025-11-29 13:06:34 +0100

    fix: type stats only reflected 30 days of data

--- api/src/State/TeaType/TeaTypeProvider.php
+++ api/src/State/TeaType/TeaTypeProvider.php
@@ -40,19 +40,16 @@ readonly class TeaTypeProvider implements ProviderInterface
 			return null;
 		}
 
-		$rsm = new ResultSetMappingBuilder($this->em)
-			->addScalarResult("rank", "rank", Types::INTEGER)
-			->addScalarResult("teas", "teas", Types::INTEGER)
-			->addScalarResult("sessions", "sessions", Types::INTEGER);
-
-		$statsQuery = $this->em->createNativeQuery(
+		$rank = $this->em->createNativeQuery(
 			<<<SQL
-			SELECT ranked.rank, ranked.teasCount as teas, ranked.sessionsCount as sessions
+			SELECT ranked.rank
 			FROM (
 				SELECT type.id as id,
-				       count(DISTINCT teas.id) as teasCount,
-				       count(DISTINCT sessions.id) as sessionsCount,
-				       ROW_NUMBER() OVER (ORDER BY count(DISTINCT sessions.id) DESC, COUNT(DISTINCT teas.id) DESC, MAX(type.created_at) DESC) as rank
+					ROW_NUMBER() OVER (ORDER BY
+						count(sessions.id) DESC,
+						COUNT(DISTINCT teas.id) DESC,
+						MAX(type.created_at) DESC
+				    ) as rank
 				FROM tea_type type
 					LEFT JOIN tea as teas ON teas.type_id = type.id
 					LEFT JOIN tea_session as sessions ON sessions.tea_id = teas.id AND sessions.drank_at >= :rankSince
@@ -60,14 +57,29 @@ readonly class TeaTypeProvider implements ProviderInterface
 			) as ranked
 			WHERE ranked.id = :typeId
 			SQL,
-			$rsm,
+			new ResultSetMappingBuilder($this->em)->addScalarResult("rank", "rank", Types::INTEGER),
 		)
 			->setParameter("typeId", $typeEntity->id)
 			->setParameter("rankSince", new \DateTimeImmutable()->sub(new \DateInterval("P1M")))
+			->getSingleScalarResult();
+
+		$stats = $this->em->createNativeQuery(
+			<<<SQL
+			SELECT count(DISTINCT teas.id) as teas, count(sessions.id) as sessions
+			FROM tea_type tea_type
+				LEFT JOIN tea as teas ON teas.type_id = tea_type.id
+				LEFT JOIN tea_session as sessions ON sessions.tea_id = teas.id
+			WHERE tea_type.id = :typeId
+			SQL,
+			new ResultSetMappingBuilder($this->em)
+				->addScalarResult("rank", "rank", Types::INTEGER)
+				->addScalarResult("teas", "teas", Types::INTEGER)
+				->addScalarResult("sessions", "sessions", Types::INTEGER),
+		)
+			->setParameter("typeId", $typeEntity->id)
 			->getSingleResult();
 
-		$resource->stats = new TeaTypeStats($statsQuery["rank"], $statsQuery["teas"], $statsQuery["sessions"]);
-
+		$resource->stats = new TeaTypeStats($rank, $stats["teas"], $stats["sessions"]);
 		return $resource;
 	}
 
