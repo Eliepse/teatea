00fea9046db8489fb4adfa9ffb31c66892a8b36f
Élie Meignan <contact@eliepse.fr>
2025-12-19 12:31:29 +0100

    feat: Date JSON serialization override

--- pwa/app/utils/time.ts
+++ pwa/app/utils/time.ts
@@ -1,3 +1,20 @@
+import { formatISO } from "date-fns";
+
+/**
+ * Create a promise that waits the given amount of ms before resolving
+ */
 export function wait(durationMs: number) {
 	return new Promise((r) => setTimeout(r, durationMs));
+}
+
+/**
+ * Makes a date serializable with JSON, including the timezone.
+ * By default, JSON serialization doesn't keep the timezone
+ */
+export function jsonableDate(date?: Date | null) {
+	if(!date) {
+		return date;
+	}
+
+	return { ...date, toJSON: () => formatISO(date) };
 }
