45df892baf7c4fc37d04c02e3a2c954c818a297e
Élie Meignan <contact@eliepse.fr>
2026-01-30 14:53:15 +0100

    fix: Remove redundant column removal

--- api/migrations/Version20260130113056.php
+++ api/migrations/Version20260130113056.php
@@ -18,6 +18,5 @@ final class Version20260130113056 extends AbstractMigration
 	public function up(Schema $schema): void
 	{
 		$this->addSql('ALTER TABLE collection_tea ADD finished_at DATE DEFAULT NULL');
-		$this->addSql('ALTER TABLE tea DROP origin_id');
 	}
 }
