0d4c6879114b363888d0aa09f46f495ea577ba99
Élie Meignan <contact@eliepse.fr>
2026-03-03 12:18:35 +0800

    fix: Return 403 when setting a duplicated username

--- api/src/State/Member/MemberOnboardingProcessor.php
+++ api/src/State/Member/MemberOnboardingProcessor.php
@@ -8,6 +8,7 @@ use App\ApiResource\Member;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * @implements ProcessorInterface<Member>
@@ -17,18 +18,30 @@ readonly class MemberOnboardingProcessor implements ProcessorInterface
 	public function __construct(
 		private EntityManagerInterface $em,
 		private Security $security,
-	) {}
+	) {
+	}
 
 	public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): Member
 	{
 		$user = $this->security->getUser();
 
 		assert($data instanceof Member);
-		assert($user instanceof User && $user->hasRole("ROLE_ONBOARDING"));
 		assert($user->id === $data->id);
 
 		$user->username = $data->username;
 		$user->setRoles(["ROLE_USER"]);
+
+		$duplicates = $this->em->createQueryBuilder()
+			->select("COUNT(u)")
+			->from(User::class, "u")
+			->where("LOWER(u.username) = LOWER(UNACCENT(:username))")
+			->setParameter("username", $user->username)
+			->getQuery()
+			->getSingleScalarResult();
+
+		if (false === empty($duplicates)) {
+			throw new HttpException(403, "Unavailable username");
+		}
 
 		$this->em->persist($user);
 		$this->em->flush();
