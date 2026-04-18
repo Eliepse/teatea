eabecb13b852cbddbc4cfe67acc53944af051ee7
Élie Meignan <contact@eliepse.fr>
2026-04-18 11:58:47 +0200

    refactor: Extract search query options and fetcher

--- pwa/app/search/hooks/useSearchQuery.ts
+++ pwa/app/search/hooks/useSearchQuery.ts
@@ -1,8 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { getApi } from "~/utils/api";
-import type { ApiPaginatedCollection, Tea, TeaFamily, TeaType } from "~t/types";
+import type { TeaFamily, TeaType } from "~t/types";
 import { createContext, useContext } from "react";
 import { throwNotImplemented } from "~/utils/function";
+import { type ItemType, makeSearchInfinitQueryOpt } from "~/search/query/searchQuery";
 
 export type SearchFilters = {
 	q?: string;
@@ -23,30 +23,8 @@ export const SE_CONTEXT = createContext<{
 	searchType: "tea_types",
 });
 
-type ItemType = "teas" | "tea_types";
-
 export function useSearchQuery(type: ItemType, filters?: SearchFilters) {
-	const query = useInfiniteQuery({
-		queryFn: async ({ queryKey, pageParam }) => {
-			let filters = queryKey[2] as SearchFilters;
-			const type = queryKey[1] as ItemType;
-
-			const originFilterNodes = filters.origin?.split(".")?.length ?? 1;
-			filters = { ...filters, distinctByLevel: originFilterNodes > 1 ? 3 : 1 };
-
-			const response = await getApi<ApiPaginatedCollection<Tea | TeaType>>(
-				pageParam ? pageParam : `/${type}`,
-				pageParam ? {} : filters,
-			);
-
-			return await response.json();
-		},
-		queryKey: ["search", type, { ...filters, itemsPerPage: 10, sort: "popularity" }],
-		getPreviousPageParam: (lastPage) => lastPage.view.previous,
-		getNextPageParam: (lastPage) => lastPage.view.next,
-		initialPageParam: "",
-	});
-
+	const query = useInfiniteQuery(makeSearchInfinitQueryOpt(type, filters));
 	return { query, isTeaTypes: "tea_types" === type, isTeas: "teas" === type };
 }
 
