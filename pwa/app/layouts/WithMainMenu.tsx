1ff8d84fa977df2f7a392328817c4381f8a696c0
Élie Meignan <contact@eliepse.fr>
2026-04-24 11:52:30 +0200

    feat: Support main menu layout on public routes

--- pwa/app/layouts/WithMainMenu.tsx
+++ pwa/app/layouts/WithMainMenu.tsx
@@ -5,15 +5,16 @@ import { CalendarDaysIcon, HomeIcon, MagnifyingGlassIcon } from "@heroicons/reac
 import { Link, useNavigation } from "react-router";
 import { EcologyBook, Leaf } from "iconoir-react";
 import { useUser } from "~/auth/hooks/useUser";
+import { IfAuthenticated } from "~/auth/components/voters/IfAuthenticated";
 
 export function WithMainMenu(props: PropsWithChildren<{ className?: string; activeKey?: string }>) {
 	const navigation = useNavigation();
-	const user = useUser();
 	const isNavigating = Boolean(navigation.location);
+	const user = useUser();
 
 	return (
 		<div className={styles.root}>
-			<div className="relative overflow-hidden">
+			<div className="relative overflow-hidden bg-green-50">
 				{isNavigating && (
 					<div className="h-full flex items-center justify-center bg-green-50">
 						<Leaf className="size-8 animate-spin text-green-700" />
@@ -25,46 +26,48 @@ export function WithMainMenu(props: PropsWithChildren<{ className?: string; acti
 				)}
 			</div>
 
-			<nav className="border-t border-gray-200">
-				<ul className="w-full h-full flex px-4">
-					<li className="flex-1">
-						<Link to="/welcome">
-							<NavItem
-								icon={<HomeIcon className="size-5" />}
-								label="Home"
-								active={"home" === props.activeKey}
-							/>
-						</Link>
-					</li>
-					<li className="flex-1">
-						<Link to={`/members/${user.data?.username}/teas`}>
-							<NavItem
-								icon={<EcologyBook className="size-5" />}
-								label="My teas"
-								active={"my-teas" === props.activeKey}
-							/>
-						</Link>
-					</li>
-					<li className="flex-1">
-						<Link to="/tea/search">
-							<NavItem
-								icon={<MagnifyingGlassIcon className="size-5" />}
-								label="Search"
-								active={"search" === props.activeKey}
-							/>
-						</Link>
-					</li>
-					<li className="flex-1">
-						<Link to="/sessions">
-							<NavItem
-								icon={<CalendarDaysIcon className="size-5" />}
-								label="Activity"
-								active={"activity" === props.activeKey}
-							/>
-						</Link>
-					</li>
-				</ul>
-			</nav>
+			<IfAuthenticated>
+				<nav className="border-t border-gray-200">
+					<ul className="w-full h-full flex px-4">
+						<li className="flex-1">
+							<Link to="/welcome">
+								<NavItem
+									icon={<HomeIcon className="size-5" />}
+									label="Home"
+									active={"home" === props.activeKey}
+								/>
+							</Link>
+						</li>
+						<li className="flex-1">
+							<Link to={`/members/${user.data?.username}/teas`}>
+								<NavItem
+									icon={<EcologyBook className="size-5" />}
+									label="My teas"
+									active={"my-teas" === props.activeKey}
+								/>
+							</Link>
+						</li>
+						<li className="flex-1">
+							<Link to="/tea/search">
+								<NavItem
+									icon={<MagnifyingGlassIcon className="size-5" />}
+									label="Search"
+									active={"search" === props.activeKey}
+								/>
+							</Link>
+						</li>
+						<li className="flex-1">
+							<Link to="/sessions">
+								<NavItem
+									icon={<CalendarDaysIcon className="size-5" />}
+									label="Activity"
+									active={"activity" === props.activeKey}
+								/>
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</IfAuthenticated>
 		</div>
 	);
 }
