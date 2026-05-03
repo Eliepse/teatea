ce4ebf5e797d41367bc23bb702a72815e7779060
Élie Meignan <contact@eliepse.fr>
2026-05-03 12:06:17 +0200

    fix: Some elements are displayed on top of menu

--- pwa/app/layouts/WithMainMenu.tsx
+++ pwa/app/layouts/WithMainMenu.tsx
@@ -13,7 +13,7 @@ export function WithMainMenu(props: PropsWithChildren<{ className?: string; acti
 	return (
 		<>
 			<IfAuthenticated>
-				<nav className="fixed bottom-0 left-0 right-0 h-16  bg-white border-t border-gray-200">
+				<nav className="fixed bottom-0 left-0 right-0 h-16 z-40 bg-white border-t border-gray-200">
 					<MainMenu />
 				</nav>
 			</IfAuthenticated>
