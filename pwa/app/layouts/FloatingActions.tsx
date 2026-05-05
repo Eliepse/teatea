381c50ee48b75d7eba2cc3ab727d303f3ddd5924
Élie Meignan <contact@eliepse.fr>
2026-05-05 17:42:20 +0200

    fix: Main menu on top of create tea flow actions
    Close #37

--- pwa/app/layouts/FloatingActions.tsx
+++ pwa/app/layouts/FloatingActions.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 
 export function FloatingActions(props: PropsWithChildren<{ className?: string }>) {
 	return (
-		<div className={clsx("fixed inset-x-4 bottom-18 flex items-center z-30", props.className)}>
+		<div className={clsx("fixed inset-x-4 bottom-18 flex items-center z-40", props.className)}>
 			{props.children}
 		</div>
 	);
--- pwa/app/layouts/WithMainMenu.tsx
+++ pwa/app/layouts/WithMainMenu.tsx
@@ -12,7 +12,7 @@ export function WithMainMenu(props: PropsWithChildren<{ className?: string; acti
 	return (
 		<>
 			<IfAuthenticated>
-				<nav className="fixed bottom-0 left-0 right-0 h-16 z-40 bg-white border-t border-gray-200">
+				<nav className="fixed bottom-0 left-0 right-0 h-16 z-30 bg-white border-t border-gray-200">
 					<MainMenu />
 				</nav>
 			</IfAuthenticated>
