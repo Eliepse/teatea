9804e222905614fe1c62eafd40c35a5562993080
Élie Meignan <contact@eliepse.fr>
2026-05-06 16:55:45 +0200

    feat: Support display selected and deselect all on family filter

--- pwa/app/catalog/components/TeaFamilyFilter.tsx
+++ pwa/app/catalog/components/TeaFamilyFilter.tsx
@@ -4,12 +4,33 @@ import { FilterButton } from "~/catalog/components/FilterButton";
 import { Family } from "~/components/tea/Family";
 import clsx from "clsx";
 
-export function TeaFamilyFilter(props: { className?: string; onSelect: (value: TeaFamily) => void }) {
+export function TeaFamilyFilter(props: {
+	selected?: TeaFamily;
+	className?: string;
+	onSelect: (value: TeaFamily|undefined) => void;
+	allChoice?: boolean,
+}) {
 	return (
 		<ul className={clsx(styles.list, "scrollbar-hide", props.className)}>
+			{props.allChoice && (
+				<li>
+					<FilterButton
+						onClick={() => props.onSelect(undefined)}
+						active={undefined === props.selected}
+						noIcon
+					>
+						All
+					</FilterButton>
+				</li>
+			)}
+
 			{Object.keys(teaFamilies).map((key) => (
 				<li key={key}>
-					<FilterButton onClick={() => props.onSelect(key as TeaFamily)} noIcon>
+					<FilterButton
+						onClick={() => props.onSelect(key as TeaFamily)}
+						active={key === props.selected}
+						noIcon
+					>
 						<Family family={key as TeaFamily} className="capitalize" />
 					</FilterButton>
 				</li>
