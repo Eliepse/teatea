298b11c9e58957d88ccd15181c2bda20f9d85970
Élie Meignan <contact@eliepse.fr>
2026-05-08 21:45:55 +0200

    feat: Add support for business on api tea creation

--- api/src/State/Tea/TeaCreateProcess.php
+++ api/src/State/Tea/TeaCreateProcess.php
@@ -14,6 +14,7 @@ use App\Message\QueryBus;
 use App\Repository\OriginRepository;
 use App\Repository\TeaRepository;
 use App\Repository\TeaTypeRepository;
+use App\State\Business\BusinessProvider;
 use App\State\Origin\OriginProvider;
 use App\State\TeaType\TeaTypeProvider;
 use Doctrine\ORM\EntityManagerInterface;
@@ -62,7 +63,8 @@ readonly class TeaCreateProcess implements ProcessorInterface
 			$type = $this->queryBus->ask(new FindTypeFamilyQuery($data->family));
 		}
 
-		$teaEntity = $this->commandBus->process(
+		/** @var \App\Entity\Tea $entity */
+		$entity = $this->commandBus->process(
 			new AddTeaCommand(
 				$type->id,
 				$data->origin?->path,
@@ -70,17 +72,21 @@ readonly class TeaCreateProcess implements ProcessorInterface
 				$data->roast,
 				$data->cultivar?->id,
 				$user->id,
+				$data->business?->id,
 			),
 		);
 
 		// Hydrate new resource
 
 		$resource = new Tea();
-		$resource->id = $teaEntity->id;
-		$resource->family = $teaEntity->family;
-		$resource->type = TeaTypeProvider::fromEntity($teaEntity->type);
-		$resource->origin = OriginProvider::fromEntity($teaEntity->origin);
-		$resource->addedAt = $teaEntity->createdAt;
+		$resource->id = $entity->id;
+		$resource->family = $entity->family;
+		$resource->type = TeaTypeProvider::fromEntity($entity->type);
+		$resource->origin = OriginProvider::fromEntity($entity->origin);
+		$resource->addedAt = $entity->createdAt;
+		$resource->roast = $entity->roast;
+		$resource->year = $entity->year;
+		$resource->business = BusinessProvider::fromEntity($entity->business);
 
 		return $resource;
 	}
