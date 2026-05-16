22cfbfd59827acac2ee56670032e9b3b5ed80579
Élie Meignan <contact@eliepse.fr>
2026-05-16 10:46:24 +0200

    feat: Support minimal resource hydration for reference use

--- api/src/State/Hydration/FriendshipHydrator.php
+++ api/src/State/Hydration/FriendshipHydrator.php
@@ -10,7 +10,7 @@ use App\State\Member\MemberProvider;
  */
 readonly class FriendshipHydrator implements ResourceHydrator
 {
-	public function hydrate(?object $entity): ?object
+	public function hydrate(?object $entity): ?Friendship
 	{
 		if (null === $entity) {
 			return null;
@@ -22,6 +22,20 @@ readonly class FriendshipHydrator implements ResourceHydrator
 		$resource->id = $entity->id;
 		$resource->requestor = MemberProvider::hydrate($entity->requestedBy);
 		$resource->requestedAt = $entity->requestedAt;
+
+		return $resource;
+	}
+
+	public function hydrateReference(?object $entity): ?Friendship
+	{
+		if (null === $entity) {
+			return null;
+		}
+
+		assert($entity instanceof \App\Entity\Pivot\Friendship);
+
+		$resource = new Friendship();
+		$resource->id = $entity->id;
 
 		return $resource;
 	}
--- api/src/State/Hydration/ResourceHydrator.php
+++ api/src/State/Hydration/ResourceHydrator.php
@@ -8,9 +8,20 @@ namespace App\State\Hydration;
 interface ResourceHydrator
 {
 	/**
+	 * Return the hydrated resource from the given enity
+	 *
 	 * @param object|null $entity
 	 *
 	 * @return TOutput|null
 	 */
 	public function hydrate(?object $entity): ?object;
+
+	/**
+	 * Return the resource with only the elements required to generate an Iri from it
+	 *
+	 * @param object|null $entity
+	 *
+	 * @return TOutput|null
+	 */
+	public function hydrateReference(?object $entity): ?object;
 }
