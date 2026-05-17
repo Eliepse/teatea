6700b5bec72def4721ec329c7ff0d7785361db86
Élie Meignan <contact@eliepse.fr>
2026-05-17 18:35:29 +0200

    feat: Optional distinction by origin on type search

--- pwa/app/utils/query/teaTypeQuery.ts
+++ pwa/app/utils/query/teaTypeQuery.ts
@@ -3,21 +3,23 @@ import { getApi } from "~/utils/api";
 import type { ApiPaginatedCollection, TeaType } from "~t/types";
 import { queryOptions } from "@tanstack/react-query";
 
-export async function queryTypesSearch(
-	filters: Pick<SearchFilters, "family" | "q" | "origin">,
-	page: { itemsPerPage?: number },
-) {
+type Filters = Pick<SearchFilters, "family" | "q" | "origin"> & { distinctOrigins?: boolean };
+
+export async function queryTypesSearch(filters: Filters, page: { itemsPerPage?: number }) {
 	const originFilterNodes = filters.origin?.split(".")?.length ?? 1;
-	const queryFilters = { ...filters, distinctByLevel: originFilterNodes > 1 ? 3 : 1 };
+	const queryFilters = {
+		...filters,
+		distinctByLevel: filters.distinctOrigins ? (originFilterNodes > 1 ? 3 : 1) : undefined,
+	};
 
 	const res = await getApi<ApiPaginatedCollection<TeaType>>("/tea_types", { ...queryFilters, ...page });
 	return await res.json();
 }
 
-export function makeTypeSearchQueryOpt(filters: Pick<SearchFilters, "family" | "q" | "origin">, limit = 8) {
+export function makeTypeSearchQueryOpt(filters: Filters, limit = 8) {
 	return queryOptions({
 		queryFn: async () => await queryTypesSearch(filters, { itemsPerPage: limit }),
 		queryKey: ["search", "tea_types", { ...filters, itemsPerPage: limit }],
-		staleTime: 60000,
+		staleTime: 60_000,
 	});
 }
