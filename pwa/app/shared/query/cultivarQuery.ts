9e16e9ce288a12d7a3bcddf2724ac6a961b2b740
Élie Meignan <contact@eliepse.fr>
2026-05-18 19:00:43 +0200

    refactor: Support iri on cultivar query

--- pwa/app/shared/query/cultivarQuery.ts
+++ pwa/app/shared/query/cultivarQuery.ts
@@ -2,17 +2,13 @@ import { queryOptions } from "@tanstack/react-query";
 import { getApi } from "~/utils/api";
 import type { Cultivar } from "~t/types";
 
-export function makeCultivarQueryOpt(id?: number) {
-	return queryOptions({
-		queryFn: async () => {
-			if (!id) {
-				return null;
-			}
+export function makeCultivarQueryOpt(cultivar?: Partial<Pick<Cultivar, "@id" | "id">>) {
+	const iri = cultivar?.["@id"] ?? `/cultivars/${cultivar?.id}`;
 
-			return await (await getApi<Cultivar>(`/cultivars/${id}`)).json();
-		},
-		queryKey: ["cultivars", id],
+	return queryOptions({
+		queryFn: async () => await (await getApi<Cultivar>(iri)).json(),
+		queryKey: ["cultivars", iri],
 		staleTime: 7 * 24 * 60_000,
-		enabled: !!id,
+		enabled: !!cultivar?.["@id"] || !!cultivar?.id,
 	});
 }
