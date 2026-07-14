7e4d92dd0384444c077b75feb6eed0485278adb4
Élie Meignan <contact@eliepse.fr>
2026-07-14 21:10:14 +0200

    utils: Improve extractId typing

--- pwa/app/utils/resource.ts
+++ pwa/app/utils/resource.ts
@@ -3,6 +3,8 @@ import type { Iri } from "~t/types";
 /**
  * Extract the ID contained in an Iri (the last part of the path)
  */
+export function extractId(iri: Iri): string;
+export function extractId(iri?: undefined): undefined;
 export function extractId(iri?: Iri): string | undefined {
 	if (undefined === iri) {
 		return undefined;
