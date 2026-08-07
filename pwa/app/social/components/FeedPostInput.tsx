a80344b08de82b82733cb923d3218765b708fa00
Élie Meignan <contact@eliepse.fr>
2026-08-07 18:19:00 +0200

    social: Add images upload

--- pwa/app/social/components/FeedPostInput.tsx
+++ pwa/app/social/components/FeedPostInput.tsx
@@ -1,47 +1,111 @@
 import clsx from "clsx";
-import { useState } from "react";
-import { Plus } from "iconoir-react";
-import { PrimaryButton } from "~/shared/components/Button";
+import { type ChangeEvent, type FocusEvent, type MouseEvent, useMemo, useRef, useState } from "react";
+import { MediaImagePlus, Plus, Xmark } from "iconoir-react";
+import { GhostButton, PrimaryButton } from "~/shared/components/Button";
 import { useMutation } from "@tanstack/react-query";
 import { makeCreatePostMutation } from "~/social/mutation/createPostMutation";
 
+interface IForm {
+	text: string;
+	files: Array<[string, File]>;
+}
+
+const EMPTY_FORM: IForm = { text: "", files: [] };
+
 export function FeedPostInput(props: { className?: string }) {
+	const containerRef = useRef<HTMLDivElement>(null);
+	const fileInputRef = useRef<HTMLInputElement>(null);
+
 	const [focused, setFocused] = useState(false);
-	const [text, setText] = useState("");
-	const active = focused || !!text;
-	const canSubmit = active && !!text.trim();
+	const [form, setForm] = useState<IForm>({ ...EMPTY_FORM });
+
+	const fileInput = useMemo(
+		() => <input ref={fileInputRef} type="file" accept="image/*" multiple onChange={handleFileUpload} hidden />,
+		[],
+	);
+
+	const active = focused || !!form.text || !!form.files.length;
+	const canSubmit = active && 3 <= form.text.trim()?.length;
 
 	const mutation = useMutation({
 		...makeCreatePostMutation(),
 		onSuccess: (post) => {
-			setText("");
+			setForm({ ...EMPTY_FORM });
 		},
 	});
 
 	function publish() {
-		if (!active || mutation.isPending || !text.trim()) {
+		if (!active || mutation.isPending || !form.text.trim()) {
 			return;
 		}
 
-		mutation.mutate({ content: text });
+		mutation.mutate({
+			content: form.text,
+			images: form.files.length ? form.files.map((entry) => entry[1]) : undefined,
+		});
+	}
+
+	function handleBlur(e: FocusEvent) {
+		if (containerRef.current?.contains(e.target)) {
+			return;
+		}
+
+		setFocused(false);
+	}
+
+	function handleClickFileBtn(e: MouseEvent) {
+		e.stopPropagation();
+		e.preventDefault();
+		fileInputRef.current?.click();
+	}
+
+	function handleFileUpload(e: ChangeEvent<HTMLInputElement>) {
+		setForm((v) => {
+			const files = Array.from(e.target.files ?? [])
+				.slice(0, 8 - v.files.length)
+				.map((file) => [URL.createObjectURL(file), file] satisfies IForm["files"][number]);
+
+			return { ...v, files: [...v.files, ...files].slice(0, 8) };
+		});
 	}
 
 	return (
 		<div
+			ref={containerRef}
 			className={clsx(
 				"duration-200 transition-all relative bg-white/80 rounded-lg mb-6 text-green-900",
 				active ? "shadow-lg/5 translate-y-1 -mx-1" : "shadow-xs",
 				props.className,
 			)}
+			onFocus={() => setFocused(true)}
+			onBlur={handleBlur}
+			tabIndex={-1}
 		>
+			{fileInput}
+
+			{!!form.files.length && (
+				<ul className="flex gap-1 px-3 pt-3">
+					{form.files.map(([src, file]) => (
+						<li
+							key={src}
+							className="relative cursor-pointer"
+							onClick={() => setForm((v) => ({ ...v, files: v.files.filter((f) => f[1] !== file) }))}
+						>
+							<span className="bg-white rounded-full absolute -top-1 -right-1 p-0.5 shadow z-1">
+								<Xmark className="w-3 h-3 block" />
+							</span>
+							<img src={src} className="w-10 h-10 rounded object-cover" />
+						</li>
+					))}
+				</ul>
+			)}
+
 			<textarea
 				className={clsx("w-full px-3 py-4 outline-none resize-none", !active && "pl-9.5")}
 				rows={1}
 				placeholder="Share your thoughts..."
-				onFocus={() => setFocused(true)}
-				onBlur={() => setFocused(false)}
-				onChange={(e) => setText(e.target.value)}
-				value={text}
+				onChange={(e) => setForm((v) => ({ ...v, text: e.target.value }))}
+				value={form.text}
 			/>
 
 			<div
@@ -53,7 +117,14 @@ export function FeedPostInput(props: { className?: string }) {
 			>
 				<hr className="mx-4 border-slate-200" />
 
-				<div className="px-4 py-2">
+				<div className="px-4 py-2 flex">
+					<GhostButton
+						icon={<MediaImagePlus />}
+						onClick={handleClickFileBtn}
+						disabled={8 <= form.files.length}
+						small
+					/>
+
 					<PrimaryButton
 						className="ml-auto"
 						loading={mutation.isPending}
--- pwa/app/social/mutation/createPostMutation.ts
+++ pwa/app/social/mutation/createPostMutation.ts
@@ -4,12 +4,13 @@ import { denormalizePost, type PostRaw } from "~/utils/api/normalization/post";
 import { postApi } from "~/utils/api";
 
 export interface IForm extends Pick<Post, "content"> {
+	images?: File[];
 }
 
 export function makeCreatePostMutation() {
 	return mutationOptions({
 		mutationFn: async (data: IForm) => {
-			const res = await postApi<PostRaw>("/api/posts", { content: data.content });
+			const res = await postApi<PostRaw>("/api/posts", { content: data.content, files: data.images });
 			return denormalizePost(await res.json());
 		}
 	});
